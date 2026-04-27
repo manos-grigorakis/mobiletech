9f52caecc43d87be08ed526f8c10edd91ac91484
manos <manos.grigorakis03@gmail.com>
2026-04-27 20:20:15 +0300

    feat: add hero section

---
+++ frontend/src/components/home/HeroSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section
+    class="flex flex-col items-center justify-center py-48 text-center text-white bg-primary-700"
+  >
+    <h1 class="text-4xl">Smartphones and mobile accessories in one place</h1>
+    <p class="mt-2 text-sm leading-relaxed">
+      Find new phones, refurbished devices, chargers, earbuds, cases, and more.
+    </p>
+    <button
+      class="px-4 py-2 mt-4 font-medium text-black rounded bg-accent-500 hover:bg-accent-600 hover:cursor-pointer"
+    >
+      Shop products
+    </button>
+  </section>
+</template>
