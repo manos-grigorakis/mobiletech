e5e5486cc82ec4551df37158363753f2e878c60f
manos <manos.grigorakis03@gmail.com>
2026-04-28 00:03:53 +0300

    feat: add why choose us section

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,10 +2,12 @@
 import FeaturedProductsSection from '@/components/home/FeaturedProductsSection.vue'
 import HeroSection from '@/components/home/HeroSection.vue'
 import ShopByCategorySection from '@/components/home/ShopByCategorySection.vue'
+import WhyChooseUsSection from '@/components/home/WhyChooseUsSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <ShopByCategorySection />
   <FeaturedProductsSection />
+  <WhyChooseUsSection />
 </template>
