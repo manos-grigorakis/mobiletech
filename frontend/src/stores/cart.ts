56232aa5c25d952b1ee0403e7a9279db5911120b
manos <manos.grigorakis03@gmail.com>
2026-05-01 20:11:52 +0300

    feat: add pinia with persistant plugin and create cart store

---
+++ frontend/src/stores/cart.ts
@@ -0,0 +1,42 @@
+import type { CartItem } from '@/types/cart-items'
+import type { Product } from '@/types/product'
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', {
+  state: () => ({
+    items: [] as CartItem[],
+  }),
+
+  persist: true,
+
+  getters: {
+    totalPrice: (state): number => state.items.reduce((total, item) => total + item.quantity, 0),
+    totalItems: (state): number => state.items.reduce((total, item) => total + item.quantity, 0),
+  },
+
+  actions: {
+    addToCart(product: Product) {
+      const existing = this.items.find((i) => i.id === product.id)
+
+      if (existing) {
+        existing.quantity++
+      } else {
+        this.items.push({ ...product, quantity: 1 })
+      }
+    },
+
+    removeFromCart(productId: number) {
+      this.items = this.items.filter((i) => i.id !== productId)
+    },
+
+    updateQuantity(productId: number, quantity: number) {
+      const item = this.items.find((i) => i.id === productId)
+
+      if (item && quantity > 0) item.quantity = quantity
+    },
+
+    clearCart() {
+      this.items = []
+    },
+  },
+})
