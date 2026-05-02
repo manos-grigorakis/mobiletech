de6e25c0532a0e1f11d11fb8ceaab575ae2e4bc6
manos <manos.grigorakis03@gmail.com>
2026-05-02 04:52:09 +0300

    feat: add main input component and libraries for form validation

---
+++ frontend/src/components/ui/MainInput.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useField } from 'vee-validate'
+
+interface Props {
+  required?: boolean
+  labelValue: string
+  inputId: string
+  inputType?: 'text' | 'email'
+  placeholder?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  required: false,
+  inputType: 'text',
+})
+
+const { value, errorMessage } = useField(() => props.inputId)
+</script>
+
+<template>
+  <div>
+    <label :for="inputId" class="block mb-1"
+      ><span v-if="required" class="mr-1 text-xs text-red-500">*</span>{{ labelValue }}</label
+    >
+    <input
+      v-model="value"
+      :type="inputType"
+      :id="inputId"
+      :placeholder="placeholder"
+      class="bg-neutral-100 border border-gray-200 text-sm rounded w-full px-3 py-2.5 shadow-xs focus:outline-none focus:border-primary-500"
+    />
+
+    <span v-if="errorMessage" class="block mt-1 text-xs text-red-500">{{ errorMessage }}</span>
+  </div>
+</template>
