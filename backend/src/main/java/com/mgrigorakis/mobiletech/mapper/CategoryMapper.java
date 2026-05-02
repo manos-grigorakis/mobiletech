6ebfc6a6617efa048c7a03e8f3a01bd024333542
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-05-02 11:56:12 +0300

    feat: add implementation for CRUD operations in Category model

---
+++ backend/src/main/java/com/mgrigorakis/mobiletech/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.mgrigorakis.mobiletech.mapper;
+
+import com.mgrigorakis.mobiletech.dto.CategoryRequest;
+import com.mgrigorakis.mobiletech.dto.CategoryResponse;
+import com.mgrigorakis.mobiletech.model.Category;
+
+public class CategoryMapper {
+    // DTO -> Model
+    public static Category toModel(CategoryRequest dto) {
+        return Category.builder()
+                .name(dto.name())
+                .slug(dto.slug())
+                .build();
+    }
+
+    // Model -> Response
+    public static CategoryResponse toResponse(Category category) {
+        return new CategoryResponse(
+                category.getId(), category.getName(), category.getName(), category.getCreatedAt(),
+                category.getUpdatedAt());
+    }
+}
---
+++ backend/src/main/java/com/mgrigorakis/mobiletech/service/CategoryService.java
@@ -0,0 +1,18 @@
+package com.mgrigorakis.mobiletech.service;
+
+import com.mgrigorakis.mobiletech.dto.CategoryRequest;
+import com.mgrigorakis.mobiletech.dto.CategoryResponse;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryResponse> getAllCategories();
+
+    CategoryResponse getCategoryById(Long id);
+
+    CategoryResponse createCategory(CategoryRequest dto);
+
+    CategoryResponse updateCategory(Long id, CategoryRequest dto);
+
+    void deleteCategoryById(Long id);
+}
