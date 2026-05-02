23a0f413d0cbdb27a4d4710e8f0bce1114580b2c
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-05-02 10:57:14 +0300

    feat: add models with their relationships

--- backend/src/main/java/com/mgrigorakis/mobiletech/model/BaseModel.java
+++ backend/src/main/java/com/mgrigorakis/mobiletech/model/BaseModel.java
@@ -1,4 +1,35 @@
 package com.mgrigorakis.mobiletech.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- backend/src/main/java/com/mgrigorakis/mobiletech/model/Category.java
+++ backend/src/main/java/com/mgrigorakis/mobiletech/model/Category.java
@@ -1,4 +1,22 @@
 package com.mgrigorakis.mobiletech.model;
 
-public class Category {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "categories")
+@Entity
+public class Category extends BaseModel {
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(name = "slug", nullable = false, unique = true, length = 50)
+    private String slug;
 }
