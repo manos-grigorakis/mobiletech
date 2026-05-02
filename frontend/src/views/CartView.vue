c04c47a2627e38700e3e527c8a2d05e67d6f68b0
manos <manos.grigorakis03@gmail.com>
2026-05-02 05:48:58 +0300

    style: update layout in cart summary with routes

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -5,11 +5,11 @@ import CartSummary from '@/components/cart/CartSummary.vue'
 <template>
   <section class="max-w-6xl px-4 py-8 mx-auto">
     <div class="grid items-start grid-cols-1 gap-6 lg:grid-cols-3">
-      <div class="lg:col-span-2">
+      <div class="order-2 lg:col-span-2 lg:order-1">
         <router-view />
       </div>
 
-      <div class="lg:col-span-1 lg:sticky lg:top-6">
+      <div class="order-1 lg:col-span-1 lg:sticky lg:top-6 lg:order-2">
         <CartSummary />
       </div>
     </div>
