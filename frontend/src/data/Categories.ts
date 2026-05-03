7fd561ae5a1de594d60cb5485c585f447b0c374d
manos <manos.grigorakis03@gmail.com>
2026-05-03 09:00:24 +0300

    refactor: update categories data to match API data for filtering

--- frontend/src/data/Categories.ts
+++ frontend/src/data/Categories.ts
@@ -1,39 +1,12 @@
 import type { Category } from '@/types/category'
 
 export const categories: Category[] = [
-  {
-    id: 1,
-    name: 'smartphones',
-    slug: 'smartphones',
-  },
-
-  {
-    id: 2,
-    name: 'refurbished phones',
-    slug: 'refurbished-phones',
-  },
-
-  {
-    id: 3,
-    name: 'chargers',
-    slug: 'chargers',
-  },
-
-  {
-    id: 4,
-    name: 'earbuds',
-    slug: 'earbuds',
-  },
-
-  {
-    id: 5,
-    name: 'power banks',
-    slug: 'power-banks',
-  },
-
-  {
-    id: 6,
-    name: 'cases & protection',
-    slug: 'cases-and-protection',
-  },
+  { id: 1, name: 'smartphones', slug: 'smartphones' },
+  { id: 2, name: 'refurbished phones', slug: 'refurbished-phones' },
+  { id: 3, name: 'chargers', slug: 'chargers' },
+  { id: 4, name: 'earbuds', slug: 'earbuds' },
+  { id: 5, name: 'Smartwatches', slug: 'Smartwatches' },
+  { id: 6, name: 'power banks', slug: 'power-banks' },
+  { id: 7, name: 'cases', slug: 'cases' },
+  { id: 8, name: 'screen protectors', slug: 'screen-protectors' },
 ]
