b11250e2e3c0a4224693062e21efad306fca1c5c
manos <manos.grigorakis03@gmail.com>
2026-05-04 17:54:55 +0300

    feat: add announcement bar

---
+++ frontend/src/components/ui/AnnouncementBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-accent-500 w-full py-2 text-center text-xs md:text-sm z-50">
+    <p>
+      <span class="font-medium">Spring Mobile Deals:</span> Free delivery for orders over €50 and up
+      to 20% off selected accessories.
+    </p>
+  </div>
+</template>
