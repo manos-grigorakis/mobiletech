2d4f056d811c6745e73164fb5ded1bc8582aeb17
manos <manos.grigorakis03@gmail.com>
2026-05-08 18:49:38 +0300

    misc: remove unused service

--- backend/src/main/java/com/mgrigorakis/mobiletech/payments/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.mgrigorakis.mobiletech.payments.service;
-
-
-import com.mgrigorakis.mobiletech.payments.dto.PaypalOrderRequest;
-
-public interface PaymentService<T, V> {
-    T createOrder(PaypalOrderRequest request);
-    V captureOrder(String orderId);
-}
