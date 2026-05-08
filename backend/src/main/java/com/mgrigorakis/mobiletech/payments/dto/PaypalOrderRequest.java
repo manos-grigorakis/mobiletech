51958fea29298eb5155e273320b9a7703ae929a1
manos <manos.grigorakis03@gmail.com>
2026-05-08 19:11:06 +0300

    misc: remove unused records for paypal integration

--- backend/src/main/java/com/mgrigorakis/mobiletech/payments/dto/PaypalOrderRequest.java
+++
@@ -1,8 +0,0 @@
-package com.mgrigorakis.mobiletech.payments.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record PaypalOrderRequest(
-        @NotNull(message = "Order ID is required")
-        Long orderId
-) {}
