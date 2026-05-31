c9138e3caa1c3de769a193668219a651b7945c41
manos <manos.grigorakis03@gmail.com>
2026-05-31 20:06:32 +0300

    feat: add refresh button to admin dashboard

--- frontend/src/views/admin/AdminCategoriesView.vue
+++ frontend/src/views/admin/AdminCategoriesView.vue
@@ -1,17 +1,47 @@
 <script setup lang="ts">
 import CategoriesTable from '@/components/admin/CategoriesTable.vue'
+import MainButton from '@/components/ui/MainButton.vue'
+import { useCategoryStore } from '@/stores/category'
+import { RefreshCw } from '@lucide/vue'
+import { ref } from 'vue'
+
+const categoryStore = useCategoryStore()
+const refreshing = ref(false)
+
+const handleRefresh = async () => {
+  refreshing.value = true
+
+  // Small delay for refresh icon animation
+  try {
+    await Promise.all([
+      categoryStore.fetchCategories(),
+      new Promise((resolve) => setTimeout(resolve, 600)),
+    ])
+  } finally {
+    refreshing.value = false
+  }
+}
 </script>
 
 <template>
   <section>
     <!-- Header -->
     <div class="flex items-center justify-between mb-6">
-      <h2 class="text-xl font-medium">Categories</h2>
-      <RouterLink
-        :to="{ name: 'admin-create-category' }"
-        class="px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-lg bg-green-500 hover:bg-green-600 hover:cursor-pointer"
-        >Create category</RouterLink
-      >
+      <h2 class="text-xl font-semibold">Categories</h2>
+
+      <div class="flex items-center gap-4">
+        <div class="w-32 -mt-2">
+          <MainButton title="Refresh" @click="handleRefresh" :disabled="refreshing">
+            <RefreshCw :size="16" :class="{ 'animate-spin': refreshing }" />
+          </MainButton>
+        </div>
+
+        <RouterLink
+          :to="{ name: 'admin-create-category' }"
+          class="px-4 py-3 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-lg bg-green-500 hover:bg-green-600 hover:cursor-pointer"
+          >Create category</RouterLink
+        >
+      </div>
     </div>
     <CategoriesTable />
   </section>
