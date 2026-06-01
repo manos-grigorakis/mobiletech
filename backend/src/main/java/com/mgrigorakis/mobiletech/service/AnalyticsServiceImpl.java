6ac7e773cfc76d0c44524df4f83e5d48fd488a3f
manos <manos.grigorakis03@gmail.com>
2026-06-01 17:26:53 +0300

    feat: add caching for analytics

--- backend/src/main/java/com/mgrigorakis/mobiletech/service/AnalyticsServiceImpl.java
+++ backend/src/main/java/com/mgrigorakis/mobiletech/service/AnalyticsServiceImpl.java
@@ -8,6 +8,7 @@ import com.mgrigorakis.mobiletech.repository.PaymentTransactionRepository;
 import com.mgrigorakis.mobiletech.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -23,42 +24,50 @@ public class AnalyticsServiceImpl implements AnalyticsService {
     private final OrderItemRepository orderItemRepository;
     private final OrderRepository orderRepository;
 
+    @Cacheable(value = "analytics", key = "'total-revenue'")
     @Override
     public ValueResponse<BigDecimal> getTotalRevenue() {
         return new ValueResponse<>(paymentTransactionRepository.getTotalRevenue(PaymentStatus.PAID));
     }
 
+    @Cacheable(value = "analytics", key = "'units-sold'")
     @Override
     public ValueResponse<Integer> getUnitsSold() {
         return orderItemRepository.getUnitsSold(PaymentStatus.PAID);
     }
 
+    @Cacheable(value = "analytics", key = "'products-to-restock'")
     @Override
     public ValueResponse<Integer> getProductsToReStock() {
         return new ValueResponse<>(productRepository.getCountOfProductsToReStock());
     }
 
+    @Cacheable(value = "analytics", key = "'stock-value'")
     @Override
     public ValueResponse<BigDecimal> getStockValue() {
         return new ValueResponse<>(productRepository.getStockValue());
     }
 
+    @Cacheable(value = "analytics", key = "'revenue-by-category'")
     @Override
     public List<RevenueByCategoryResponse> getRevenueByCategory() {
         return orderItemRepository.getRevenueByCategory(PaymentStatus.PAID);
     }
 
+    @Cacheable(value = "analytics", key = "'monthly-sales-trend'")
     @Override
     public List<MonthlySalesTrendResponse> getMonthlySalesTrend() {
         return paymentTransactionRepository.getMonthlySalesTrend(PaymentStatus.PAID);
     }
 
+    @Cacheable(value = "analytics", key = "'top-selling-products-' + #limit")
     @Override
     public List<TopSellingProductResponse> getTopSellingProducts(int limit) {
         return orderItemRepository
                 .getTopSellingProducts(PaymentStatus.PAID, PageRequest.of(0, limit));
     }
 
+    @Cacheable(value = "analytics", key = "'orders-by-status'")
     @Override
     public List<OrderByStatusResponse> getOrdersByStatus() {
         return orderRepository.getOrderByStatus();
